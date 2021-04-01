97bba9f87fb6f30e1d34f8d3da0ae358c25d315f
PaRus <PaRusakovich@gmail.com>
2021-04-01 15:12:21 +0300

    Добавьте файлы проекта.

---
+++ Work/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstname = "Pavel";
+            var lastname = "Rusakovich";
+            var fullname = firstname + " " + lastname;
+            var myfullname = string.Format("My name is {0} {1}", firstname, lastname);
+
+            var names = new string[3] { "John", "Jack", "mary" };
+            var formattednames = string.Join(",", names);
+
+
+
+            var text = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+       Console.WriteLine(text);
+        }
+
+    }
+}
